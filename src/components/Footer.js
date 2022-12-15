4feb559657933958cf5729fc1fb0001b63b1c09e
Serjay Parks <75953992+iLOVEthecolorORANGE@users.noreply.github.com>
2022-12-15 18:31:23 -0500

    React act created

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+
+
+export default function Footer(props) {
+    return (
+      
+        <footer>
+      <nav>
+        <ul>
+            <li><a class="bottomNav" href="#">Home</a></li>
+            <li><a class="bottomNav" href="#">Women's</a></li>
+            <li><a class="bottomNav" href="#">Mens's</a></li>
+            <li><a class="bottomNav" href="#">On the Street</a></li>
+            <li><a class="bottomNav" href="#">The catwalk</a></li>
+            <li><a class="bottomNav" href="#">AdWatch</a></li>
+            <li><a class="bottomNav" href="#">About</a></li>
+            <li><a class="bottomNav" href="#">Tips</a></li>
+        </ul>
+      </nav>
+      <h4>&copy; 2013Valet Industries, Inc</h4>
+    </footer>
+      
+    );
+  }
---
+++ src/components/Nav.js
@@ -0,0 +1,40 @@
+export default function Nav(props) {
+  return (
+    <>
+      <nav aria-label="Main Navigation" role="navigation">
+        <ul>
+          <li>
+            <a class="topNav" href="#">
+              Women's
+            </a>
+          </li>
+          <li>
+            <a class="topNav" href="#">
+              Mens's
+            </a>
+          </li>
+          <li>
+            <a class="topNav" href="#">
+              On the Street
+            </a>
+          </li>
+          <li>
+            <a className="topNav" href="#">
+              The catwalk
+            </a>
+          </li>
+          <li>
+            <a className="topNav" href="#">
+              AdWatch
+            </a>
+          </li>
+          <li>
+            <a className="topNav" href="#">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
